6a344dfd4f18a6ea6af48e2f04f03fafbe5a85b1
ElienigenaChikito <eliasnoutilizado@gmail.com>
2025-03-08 14:15:01 +0100

    aa

--- ft_itoa.c
+++ ft_itoa.c
@@ -11,3 +11,50 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	ft_numlen(int n)
+{
+	int		count;
+	long	num;
+
+	count = 1;
+	num = n;
+	if (num < 0)
+	{
+		count++;
+		num = -num;
+	}
+	while (num >= 10)
+	{
+		num /= 10;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		size;
+	long	num;
+
+	num = n;
+	size = ft_numlen(n);
+	str = (char *)malloc(sizeof(char) * (size + 1));
+	if (!str)
+		return (NULL);
+	str[size] = '\0';
+	if (num < 0)
+	{
+		str[0] = '-';
+		num = -num;
+	}
+	while (--size >= 0)
+	{
+		if (str[size] == '-')
+			break ;
+		str[size] = (num % 10) + '0';
+		num /= 10;
+	}
+	return (str);
+}
