3f9edc8436a537892fffbc3a59bc0dcca2b2e78c
ElienigenaChikito <eliasnoutilizado@gmail.com>
2025-03-12 14:39:39 +0100

    not ended yet

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -1,32 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eoteros- <eoteros-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/09 13:39:31 by eoteros-          #+#    #+#             */
-/*   Updated: 2025/03/09 15:02:22 by eoteros-         ###   ########.fr       */
+/*   Created: 2025/03/11 17:14:43 by eoteros-          #+#    #+#             */
+/*   Updated: 2025/03/11 17:14:43 by eoteros-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strmapi(const char *s, char (*f)(unsigned int, char))
+void    ft_putchar_fd(char c, int fd)
 {
-    char    *str;
-    int     i;
-    if (!s || !f)
-        return (NULL);
-    str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
-    if (!str)
-        return (NULL);
-    i = 0;
-    while (s[i])
-    {
-        str[i] = f(i, s[i]);
-        i++;
-    }
-    str[i] = '\0';
-    return (str);
+    write(fd, &c, 1);       
 }
--- ft_striteri.c
+++ ft_striteri.c
@@ -1,32 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eoteros- <eoteros-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/09 13:39:31 by eoteros-          #+#    #+#             */
-/*   Updated: 2025/03/09 15:02:22 by eoteros-         ###   ########.fr       */
+/*   Created: 2025/03/11 16:48:40 by eoteros-          #+#    #+#             */
+/*   Updated: 2025/03/11 16:48:40 by eoteros-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strmapi(const char *s, char (*f)(unsigned int, char))
+void    ft_striteri(char *s, void  (*f)(unsigned int, char*))
 {
-    char    *str;
-    int     i;
+    unsigned int i;
+
     if (!s || !f)
-        return (NULL);
-    str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
-    if (!str)
-        return (NULL);
+        return;
     i = 0;
     while (s[i])
     {
-        str[i] = f(i, s[i]);
+        f(i, &s[i]);
         i++;
     }
-    str[i] = '\0';
-    return (str);
 }
