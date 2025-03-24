69980005a412df447f9198db2579b0394735688e
ElienigenaChikito <eliasnoutilizado@gmail.com>
2025-03-24 15:28:15 +0100

    Funciones de listas

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,13 +6,13 @@
 /*   By: eoteros- <eoteros-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 14:07:41 by eoteros-          #+#    #+#             */
-/*   Updated: 2025/03/24 14:21:01 by eoteros-         ###   ########.fr       */
+/*   Updated: 2025/03/24 14:36:34 by eoteros-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void (*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_node;
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: eoteros- <eoteros-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 13:36:03 by eoteros-          #+#    #+#             */
-/*   Updated: 2025/02/20 13:47:37 by eoteros-         ###   ########.fr       */
+/*   Updated: 2025/02/20 16:17:41 by eoteros-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,18 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
-	size_t	i;
-	size_t	j;
+	size_t	s_len;
 
 	if (s == NULL)
 		return (NULL);
-	if (start >= ft_strlen(s))
+	s_len = ft_strlen(s);
+	if (start >= s_len)
 		return (ft_strdup(""));
-	if (start > ft_strlen(s))
-	{
-		i = 0;
-		while (s[i++] != '\0')
-		{
-			if (i == start)
-			{
-				while (len-- > 0)
-				{
-					sub[j] = s[i];
-					i++;
-					j++;
-				}
-			}
-		}
-	}
+	if (len > s_len - start)
+		len = s_len - start;
+	sub = (char *)malloc(sizeof(char) * (len + 1));
+	if (sub == NULL)
+		return (NULL);
+	ft_strlcpy(sub, s + start, len + 1);
 	return (sub);
 }
