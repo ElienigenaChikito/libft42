9083841c66cdd8c3ecef980465dc4e208b7cd909
ElienigenaChikito <eliasnoutilizado@gmail.com>
2025-03-25 19:52:31 +0100

    ended, all good

--- ft_strdup.c
+++ ft_strdup.c
@@ -29,3 +29,25 @@ char	*ft_strdup(const char *s1)
 	copia[i] = '\0';
 	return (copia);
 }
+// Crea una copia de una str. Utiliza malloc.
+//
+//
+/*
+														||
+char	*ft_strdup(const char *s1)						||
+{														||//Creamos memoria para copia con el
+	char	*copia = (char *) malloc(ft_strlen(s1) + 1);|| tamano suficiente + '\0' para
+	size_t	i = 0;										|| copiar la cadena.
+														||
+	if (copia == NULL)									||//Si no hay memoria, retorna NULL.
+		return (NULL);									||
+	while (s1[i] != '\0')								||//Iteramos en la str, copiando
+	{													|| caracter por caracter la str.
+		copia[i] = s1[i];								||
+		i++;											||//Terminamos la copia con '\0'.
+	}													||
+	copia[i] = '\0';									||//Retornamos la copia.
+	return (copia);										||
+}														||
+														||
+*/
