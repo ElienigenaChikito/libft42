3afd96511c3adef5f4ad69786b9d3da03b63457f
ElienigenaChikito <eliasnoutilizado@gmail.com>
2025-03-29 14:32:33 +0100

    ended, all good

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -23,3 +23,21 @@ void	ft_putstr_fd(char *s, int fd)
 		}
 	}
 }
+// Escribe una Str, en el "file" indicado.
+// 
+//
+/*
+										||
+void	ft_putstr_fd(char *s, int fd)	||
+{										||//Comprobamos que haya Str.
+	if (s)								||
+	{									||//Mientras recorremos la str, en el file designado,
+		while (*s)						|| escribimos la Str.
+		{								|| s++: para incrementar y recorer la str.
+			write(fd, s, 1);			||
+			s++;						||
+		}								||
+	}									||
+}										||
+										||
+*/
--- ft_split.c
+++ ft_split.c
@@ -109,3 +109,109 @@ char	**ft_split(const char *s, char c)
 		return (NULL);
 	return (array);
 }
+// Practicamente, la funcion mas dificil de la pelicula:
+// (1)(wordcount) Retorna el conteo de palabras, separando por el separador.
+// (2)(putword) Esta funcion es la que escribe palabra por palabra.
+// (3)(free) Libera memoria cuando hay error, para proteger el codigo.
+// (4)(fill_array) Utiliza (2) para rellenar una segunda str, donde guardaremos la str dividida.
+// (5)(split) Crea una segunda str y utiliza (4)
+//
+/*
+
+static	int	ft_wordcount(const char *s, char c)					||
+{																||
+	int	count = 0;												||(1)___wordcount________________________
+	int	in_word = 0;											||
+																||//Creamos un contador.
+	while (*s)													||//Creamos un iterador.
+	{															||
+		if (*s != c && in_word == 0)							||//Mientras recorremos la Str.
+		{														||  //Si no esta dentro de una palabra,
+			count++;											||   y la letra no es un separador, cuenta.
+			in_word = 1;										||	//Si la letra, es el separador, indica
+		}														||	 que estas dentro de palabra.
+		else if (*s == c)										||
+			in_word = 0;										||//Retorna el conteo
+		s++;													||
+	}															||(2)___putword___________________________
+	return (count);												||
+}																||//Creamoos variable donde asignaremos la
+// (1)															|| palabra.
+																||//Calculamos el tamano de la palabra.
+static char	*ft_putword(const char *s, char c)					||//Creamos iterador.
+{																||
+	char	*word;												||//calculamos el tamano de la palabra.
+	int		wordlen = 0;										||
+	int		i = 0;												||//Asignamos memoria con el tamano adecuado.
+																||//Si no se ha podido asignar memoria, return.
+	while (s[wordlen] && s[wordlen] != c)						||
+		wordlen++;												||//Mientras iterador sea menor a la longitud.
+	word = (char *)malloc(sizeof(char) * (wordlen + 1));		||//Copiamos la palabra en la variable creada.
+	if (!word)													||
+		return (NULL);											||//Terminamos la palabra en Nulo.
+	while (i < wordlen)											||
+	{															||//Retornamos la palabra alojada.
+		word[i] = s[i];											||
+		i++;													||(3)___free_______________________________
+	}															||
+	word[i] = '\0';												||//Creamos un iterador.
+	return (word);												||
+}																||//Mientras hayan palabras, 
+//  (2)															||
+																||//Genera un Free, que va limpiando cada
+static void	ft_free(char **array, int words)					|| palabra de dentro de ese array.
+{																||
+	int	i = 0;													||//Libera el espacio de antes.
+																||
+	while (i < words)											||
+	{															||(4)___fill_array_________________________
+		free(array[i]);											||
+		i++;													||//Creamos un iterador.
+	}															||
+	free(array);												||//Mientras exista una palabra que rellenar,
+}																||
+//  (3)															|| //Si hay separador, continua.
+																||
+static int	ft_fill_array(char **array, const char *s, char c)	||//Si hay palabra:
+{																||//en el primer espacio pon la primera palabra,
+	int	i = 0;													|| //Si no se pudo rellenar, libera memoria.
+																||//Pasa a la siguiente palabra.
+	while (*s)													||
+	{															||//Termina la str en nulo '\0'.
+		while (*s == c)											||
+			s++;												||
+		if (*s)													||(5)___split________________________________
+		{														||
+			array[i] = ft_putword(s, c);						||//Creamos una variable en la que asignaremos
+			if (!array[i])										|| memoria, para meter la frase recortada.
+			{													||
+				ft_free(array, i);								||//Contamos las palabras, para saber cuanto
+				return (0);										|| debemos asignar.
+			}													||
+			i++;												||//Si no hay string, retorna.
+			while (*s && *s != c)								||
+				s++;											||//Asignas memoria, contando cuantas palabras
+		}														|| hay y un nulo extra.
+	}															||
+	array[i] = NULL;											||//Si no se ha podido asignar, retorna.
+	return (1);													||
+}																||//Haz la funcion (4), y devuelve array
+// (4)															|| //Y si la funcion falla, retorna NULL.
+																||
+char	**ft_split(const char *s, char c)						||//Devuelve la funcin dividida.
+{																||
+	char	**array;											||
+	int		word_count = ft_wordcount(s, c);					||
+																||
+	if (!s)														||
+		return (NULL);											||
+	array = (char **)malloc(sizeof(char *) * (word_count + 1));	||
+	if (!array)													||
+		return (NULL);											||
+	if (!ft_fill_array(array, s, c))							||
+		return (NULL);											||
+	return (array);												||
+}																||
+//  (5)															||
+																||
+*/
